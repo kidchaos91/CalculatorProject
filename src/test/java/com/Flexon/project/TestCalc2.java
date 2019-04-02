b31d05b8b494a1b6d89de52b19a9555c6f15600c
unknown <shivam.kid.chaos@gmail.com>
2019-04-01 22:58:21 -0700

    sixth commit

--- src/test/java/com/Flexon/project/TestCalc2.java
+++ src/test/java/com/Flexon/project/TestCalc2.java
@@ -1,8 +1,11 @@
 package com.Flexon.project;
 
-import static org.junit.Assert.assertEquals;
+import org.testng.Assert;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class TestCalc2 {
 	@Test
@@ -12,8 +15,33 @@ public class TestCalc2 {
 		Integer y = 15;
 		Integer expResult = 30;
 		Integer result = Calculator.sum(x, y);
-		assertEquals(null, Calculator.sum(15, null));
+		Assert.assertEquals(null, Calculator.sum(15, null));
 		System.out.println(result);
 	}
+	
+	@Test
+	public void testSum3() {
+		System.out.println("sum");
+		Integer x = -15;
+		Integer y = 15;
+		Integer expResult = 30;
+		Integer result = Calculator.sum(x, y);
+		Assert.assertEquals(null, Calculator.sum(15, null));
+		System.out.println(result);
+	}
+	
+	@BeforeMethod
+	public void beforeTheMethod() {
+		System.out.println("before method");
+	}
+	
+	@BeforeTest
+	public void beforeTheTest() {
+		System.out.println("before");
+	}
 
+	@AfterTest
+	public void afterTheTest() {
+		System.out.println("after");
+	}
 }
